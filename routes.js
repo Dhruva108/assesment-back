335b446bf91ce3229b39b4015c44bd1e8da35690
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-22 21:42:34 -0500

    feat: re organize routes

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
+const router = require('./api/healthcheck/index');
+const user = require('./api/user/index');
+
 function routes(api) {
-  api.use('api/users');
-  api.use('api/users');
+  api.use('api/users', router);
+  api.use('api/users', user);
 }
 
 module.exports = routes;
