a63f0f55fefa7d4a9a8d7cb1089e7748406eeef1
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-23 12:07:20 -0500

    feat: fix routes

--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ const router = require('./api/healthcheck/index');
 const user = require('./api/user/index');
 
 function routes(api) {
-  api.use('api/healthcheck', router);
-  api.use('api/users', user);
+  api.use('/api/healthcheck', router);
+  api.use('/api/user', user);
 }
 
 module.exports = routes;
